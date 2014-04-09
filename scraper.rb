849d46f6a0730caa3a4be2c190fe8ef3f169c1f1
Matthew Landauer <matthew@openaustralia.org>
2014-04-09 10:48:25 +1000

    Save to database

--- scraper.rb
+++ scraper.rb
@@ -90,7 +90,12 @@ class WollongongScraper
         "comment_url" => enquiry_url,
         "date_scraped" => Date.today.to_s
       }
-      p record
+      #p record
+      if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
+        ScraperWiki.save_sqlite(['council_reference'], record)
+      else
+        puts "Skipping already saved record " + record['council_reference']
+      end
     end
   end
 end
