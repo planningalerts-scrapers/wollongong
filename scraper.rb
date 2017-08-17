4c013fc87d62aa93a9f66d002231db4473623479
Eric Tam <eric.tam@traceyanderic.com>
2017-08-17 14:01:02 +1000

    improved console output

--- scraper.rb
+++ scraper.rb
@@ -85,6 +85,7 @@ class WollongongScraper
       }
       #p record
       if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
+        puts "Saving record " + record['council_reference'] + " - " + record['address']
         ScraperWiki.save_sqlite(['council_reference'], record)
       else
         puts "Skipping already saved record " + record['council_reference']
