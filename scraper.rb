4ae7bf60b1805113d8e2d64d8792401579febf7d
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 12:11:28 +1000

    Upgrade to latest version of the gem

--- scraper.rb
+++ scraper.rb
@@ -2,5 +2,5 @@ require "epathway_scraper"
 
 EpathwayScraper.scrape_and_save(
   "http://epathway.wollongong.nsw.gov.au/ePathway/Production",
-  list_type: :advertising
+  list_type: :advertising, state: "NSW"
 )
